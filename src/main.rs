031eeb62efe9be0ae534ea8018362c028621323a
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2024-02-17 12:07:17 +0530

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,4 @@
-use axum::{
-    extract::Path,
-    http::StatusCode,
-    routing::{get, post},
-    Router,
-};
+use axum::{extract::Path, http::StatusCode, routing::get, Router};
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
@@ -13,8 +8,8 @@ async fn fake_error() -> StatusCode {
     StatusCode::INTERNAL_SERVER_ERROR
 }
 
-async fn nested_post(Path(first): Path<u32>) -> StatusCode {
-    StatusCode::CREATED
+async fn nested_xor(Path((first, second)): Path<(u32, u32)>) -> String {
+    (u32::pow(first ^ second, 3)).to_string()
 }
 
 #[shuttle_runtime::main]
@@ -22,7 +17,7 @@ async fn main() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
         .route("/", get(hello_world))
         .route("/-1/error", get(fake_error))
-        .route("/1/:first/:second", post(nested_post));
+        .route("/1/:first/:second", get(nested_xor));
 
     Ok(router.into())
 }
